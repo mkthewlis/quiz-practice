125678ac06a13218396219ff660170aba3bf61d1
mkthewlis <mkthewlis@gmail.com>
2020-06-14 13:55:47 +0000

    docs: added feature to record results of the test and to log them to the console

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,16 +1,29 @@
 var correct = document.getElementsByClassName("true");
 var incorrect = document.getElementsByClassName("false");
+var result = document.getElementById("submit-button");
+
+let results = 0;
 
 //Prints 'Correct' to the console when clicked
 $('.true').click(function() {
     console.log('Correct!');
+    this.append(' Correct!');
+    results ++;
 });
 
 //Prints alternative message to the console when clicked
 $('.false').click(function(){
     console.log('Sorry, that is not correct!');
+    this.append(' Sorry, that is not correct!');
 });
 
 $('.true.bonus').click(function(){
     console.log('Bonus point, well done!');
+    results ++
 });
+
+$('#submit-button').click(function(){
+    console.log('Your score is: ' + results);
+});
+
+// Source for guidance: https://stackoverflow.com/questions/29022829/increment-score-using-javascript
