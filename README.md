e656ff877a9369a7c504136d7cfa5f06c0d0185f
mkthewlis <mkthewlis@gmail.com>
2020-06-14 14:25:08 +0000

    docs: added image to quiz, updated readme.md and altered the way that the user views the results

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-I set this repository up to practice building a basic quiz with JavaScript. 
+I set this repository up to practice building a basic quiz with jQuery and JavaScript. 
 
 This was particularly useful before setting up my second Milestone Project with The Code Institute.
+
+The image is owned by and sourced from The Ocean CleanUp: https://theoceancleanup.com/
 
 The live basic project can be viewed here: https://mkthewlis.github.io/quiz-practice/
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,29 +1,35 @@
-var correct = document.getElementsByClassName("true");
-var incorrect = document.getElementsByClassName("false");
-var result = document.getElementById("submit-button");
-
+//Set results at zero at the beginning of the quiz
 let results = 0;
+
+//Only runs function when page is ready
+$(document).ready(function(){
 
 //Prints 'Correct' to the console when clicked
 $('.true').click(function() {
     console.log('Correct!');
-    this.append(' Correct!');
+    this.append(' (Correct!)');
     results ++;
 });
 
 //Prints alternative message to the console when clicked
 $('.false').click(function(){
     console.log('Sorry, that is not correct!');
-    this.append(' Sorry, that is not correct!');
+    this.append(' (Sorry, that is not correct!)');
 });
 
+//Adds an extra point for the bonus question
 $('.true.bonus').click(function(){
     console.log('Bonus point, well done!');
+    this.append(' (Bonus point, well done!)');
     results ++
 });
 
+// Prints the results under the submit button
 $('#submit-button').click(function(){
     console.log('Your score is: ' + results);
+    $('#results').append('Your score is: ' + results + '/7 points');
+});
+
 });
 
 // Source for guidance: https://stackoverflow.com/questions/29022829/increment-score-using-javascript
