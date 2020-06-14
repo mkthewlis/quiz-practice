8fc194cb6fb4341ba31874055510f26853a11631
mkthewlis <mkthewlis@gmail.com>
2020-06-14 12:38:22 +0000

    docs: added classes and heading to quiz, added a function to console.log 'hello' to test page set up correctly

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,3 +1,20 @@
-let quizSection = document.getElementById('quiz-practice');
+let oneA = document.getElementById('one-a');
+
+var correct = document.getElementsByClassName('true');
+var incorrect = document.getElementsByClassName('false');
+
+incorrect.onclick = function(){
+    console.log('Incorrect');
+};
+
+correct.addEventListener = function(){
+    console.log('Correct');
+};
+
+oneA.onclick = function(){
+    console.log('Hello');
+};
+
+/*let quizSection = document.getElementById('quiz-practice');
 let resultsSection = document.getElementById('results');
-let submitButton = document.getElementById('submit-button');
+let submitButton = document.getElementById('submit-button');*/
